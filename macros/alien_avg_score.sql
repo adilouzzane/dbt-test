595cda2792ede463e3a31dacddfe3ffdd99f7e2b
Adil Ouzzane <adil.ouzzane@gmail.com>
2025-06-21 22:46:27 +0000

    adding snapshots and unit_tests

--- macros/alien_avg_score.sql
+++ macros/alien_avg_score.sql
@@ -1,12 +1,15 @@
 {% macro avg_score(strength_level, speed_level, intelligence, decimal_places=2) %}
 
-    round(
-            (
-                cast(strength_level as decimal) +
-                cast(speed_level as decimal) +
-                cast(intelligence as decimal)
-            ) / 3,
-            {{ decimal_places }}
-        )
+    CAST(
+        round(
+                (
+                    cast(strength_level as decimal) +
+                    cast(speed_level as decimal) +
+                    cast(intelligence as decimal)
+                )::numeric / 3.0,
+                {{ decimal_places }}
+            )
+        AS NUMERIC(10, 2)
+    )
 
 {% endmacro %}

754fe94cfef61c28381cd4fecfc54a91fc9d6769
Adil Ouzzane <adil.ouzzane@gmail.com>
2025-06-24 21:28:28 +0000

    adding model contract to dim_aliens

--- models/marts/dim_aliens.sql
+++ models/marts/dim_aliens.sql
@@ -13,11 +13,13 @@ battles as (
         max(battle_date) as last_battle_date,
         count(1) as total_battles,
         sum(CASE WHEN alien_name = winner THEN 1 ELSE 0 END) AS won_battles,
-        count(distinct enemy_name) AS fought_emenies
+        count(distinct enemy_name) AS fought_emenies,
+        CASE WHEN alien_name = winner THEN true ELSE false END AS is_winner
     from 
         {{ ref('stg_ben10__battles') }}
     group by
-        alien_name
+        alien_name,
+        winner
 ),
 
 
@@ -25,7 +27,14 @@ battles as (
 
 final AS (
     select
-        *
+        alien_id,
+        alien_name,
+        fist_battle_date,
+        last_battle_date,
+        total_battles,
+        won_battles,
+        fought_emenies,
+        is_winner
     from
         aliens
     left join battles using (alien_name)

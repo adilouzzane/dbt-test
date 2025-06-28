1546c073b7cc09d1c99da2ce63e02006feec44de
Adil Ouzzane <adil.ouzzane@gmail.com>
2025-06-28 11:51:30 +0000

    migration : adding sources

--- models/legacy/customer_orders.sql
+++ models/legacy/customer_orders.sql
@@ -1,26 +1,26 @@
-WITH paid_orders as (select Orders.ID as order_id,
-    Orders.USER_ID	as customer_id,
-    Orders.ORDER_DATE AS order_placed_at,
-        Orders.STATUS AS order_status,
+WITH paid_orders as (select Orders."ID" as order_id,
+    Orders."USER_ID"	as customer_id,
+    Orders."ORDER_DATE" AS order_placed_at,
+        Orders."STATUS" AS order_status,
     p.total_amount_paid,
     p.payment_finalized_date,
-    C.FIRST_NAME    as customer_first_name,
-        C.LAST_NAME as customer_last_name
+    C."FIRST_NAME"    as customer_first_name,
+        C."LAST_NAME" as customer_last_name
 FROM dbt_aj2b.jaffle_shop.orders as Orders
-left join (select ORDERID as order_id, max(CREATED) as payment_finalized_date, sum(AMOUNT) / 100.0 as total_amount_paid
+left join (select "ORDERID" as order_id, max("CREATED") as payment_finalized_date, sum(cast("AMOUNT" as integer)) / 100.0 as total_amount_paid
         from dbt_aj2b.stripe.payment
-        where STATUS <> 'fail'
-        group by 1) p ON orders.ID = p.order_id
-left join dbt_aj2b.jaffle_shop.customers C on orders.USER_ID = C.ID ),
+        where "STATUS" <> 'fail'
+        group by 1) p ON orders."ID" = p.order_id
+left join dbt_aj2b.jaffle_shop.customers C on orders."USER_ID" = C."ID" ),
 
 customer_orders 
-as (select C.ID as customer_id
-    , min(ORDER_DATE) as first_order_date
-    , max(ORDER_DATE) as most_recent_order_date
-    , count(ORDERS.ID) AS number_of_orders
+as (select C."ID" as customer_id
+    , min("ORDER_DATE") as first_order_date
+    , max("ORDER_DATE") as most_recent_order_date
+    , count(ORDERS."ID") AS number_of_orders
 from dbt_aj2b.jaffle_shop.customers C 
 left join dbt_aj2b.jaffle_shop.orders as Orders
-on orders.USER_ID = C.ID 
+on orders."USER_ID" = C."ID" 
 group by 1)
 
 select
---
+++ models/marts/migration/fct_customer_orders.sql
@@ -0,0 +1,66 @@
+-- Import CTE
+-- Logic CTE
+-- Final CTE
+with
+    paid_orders as (
+        select
+            orders."ID" as order_id,
+            orders."USER_ID" as customer_id,
+            orders."ORDER_DATE" as order_placed_at,
+            orders."STATUS" as order_status,
+            p.total_amount_paid,
+            p.payment_finalized_date,
+            c."FIRST_NAME" as customer_first_name,
+            c."LAST_NAME" as customer_last_name
+        from {{ source('jaffle_shop', 'orders') }} as orders
+        left join
+            (
+                select
+                    "ORDERID" as order_id,
+                    max("CREATED") as payment_finalized_date,
+                    sum(cast("AMOUNT" as integer)) / 100.0 as total_amount_paid
+                from {{ source('stripe', 'payment') }}
+                where "STATUS" <> 'fail'
+                group by 1
+            ) p
+            on orders."ID" = p.order_id
+        left join {{ source('jaffle_shop', 'customers') }} c on orders."USER_ID" = c."ID"
+    ),
+
+    customer_orders as (
+        select
+            c."ID" as customer_id,
+            min("ORDER_DATE") as first_order_date,
+            max("ORDER_DATE") as most_recent_order_date,
+            count(orders."ID") as number_of_orders
+        from {{ source('jaffle_shop', 'customers') }} c
+        left join {{ source('jaffle_shop', 'orders') }} as orders on orders."USER_ID" = c."ID"
+        group by 1
+    )
+
+select
+    p.*,
+    row_number() over (order by p.order_id) as transaction_seq,
+    row_number() over (
+        partition by customer_id order by p.order_id
+    ) as customer_sales_seq,
+    case
+        when c.first_order_date = p.order_placed_at then 'new' else 'return'
+    end as nvsr,
+    x.clv_bad as customer_lifetime_value,
+    c.first_order_date as fdos
+from paid_orders p
+left join customer_orders as c using (customer_id)
+left outer join
+    (
+        select p.order_id, sum(t2.total_amount_paid) as clv_bad
+        from paid_orders p
+        left join
+            paid_orders t2
+            on p.customer_id = t2.customer_id
+            and p.order_id >= t2.order_id
+        group by 1
+        order by p.order_id
+    ) x
+    on x.order_id = p.order_id
+order by order_id
